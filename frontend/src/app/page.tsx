8b6b9c7391348a03ae4d14ced08faa6b2bc637f8
Arnab Banik <bsse1230@iit.du.ac.bd>
2024-10-27 22:29:38 +0600

    style: initialize frontend #1

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <>
+      <Button variant="default" size="lg">Button</Button>
+      Some text to test out some stuff and default font 
+    </>
+  );
+}
